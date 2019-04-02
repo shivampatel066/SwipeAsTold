6cdfcca696e04f858bc2d2550d4f0bd472e6eae0
Shivam Patel <shivampatel066@gmail.com>
2019-04-02 18:20:26 +0530

    Initial Commit

---
+++ SwipeAsTold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeAsTold
+//
+//  Created by Shivam on 02/04/19.
+//  Copyright © 2019 Shivam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
