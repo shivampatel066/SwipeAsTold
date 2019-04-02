b3cf4894a274b913e3cf96e2282dc5500efced7c
Shivam Patel <shivampatel066@gmail.com>
2019-04-02 18:45:18 +0530

    UI ready for SwipeAsTold Game !!

--- SwipeAsTold/ViewController.swift
+++ SwipeAsTold/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var simonLabel: UILabel!
+    
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    var timer = Timer()
+    var simonTimer = Timer()
+    
+    var timeInt = 20
+    var scoreInt = 0
+    var modeInt = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        simonLabel.layer.cornerRadius = 15
+        
+        
     }
 
 
+    
+    @IBAction func startGame(_ sender: Any) {
+    }
+    
+    
 }
 
